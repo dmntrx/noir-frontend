e24a7adbfb7669c24feda916c55e8a0d0d0521dc
dmntrx <maryshkuran@mail.ru>
2025-06-13 00:35:10 +0300

    Profile page: ui - stub

--- react-vite-project/src/pages/Profile.jsx
+++ react-vite-project/src/pages/Profile.jsx
@@ -1,5 +1,23 @@
+import './Profile.css';
+
 export default function Profile() {
+    // временная заглушка на пользователя
+    const user = {
+        username: 'User1',
+        email: 'user1@example.com',
+    };
+
     return (
-        <h1>PROFILE</h1>
+        <>
+            <h1>PROFILE</h1>
+            <div className="profile-info">
+                <p><strong>Username:</strong> {user.username}</p>
+                <p><strong>Email:</strong> {user.email}</p>
+            </div>
+            <div className="profile-actions">
+                <button className="logout-btn">Log out</button>
+                <buttom className="delete-btn">Delete Account</buttom>
+            </div>
+        </>
     )
 }
