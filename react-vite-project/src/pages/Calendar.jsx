822fa243f9a49d829155669bb99fd43a9774b98d
dmntrx <maryshkuran@mail.ru>
2025-06-13 00:10:27 +0300

    Calendar page: fullCalendar, simple prompt interaction (ui in-memory)

--- react-vite-project/src/pages/Calendar.jsx
+++ react-vite-project/src/pages/Calendar.jsx
@@ -1,5 +1,40 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { useState } from 'react';
+import './Calendar.css';
+
 export default function Calendar() {
+    // текущее состояние (массив событий), ф-я изменения этого состояния
+    const [events, setEvents] = useState([
+        { title: 'Event 1', date: '2025-06-17' }
+    ]);
+
+    const handleDateClick = (info) => {
+        const title = prompt('Add event');
+        if (title) {
+            setEvents([...events, { title, date: info.dateStr }]);
+        }
+    };
     return (
-        <h1>CALENDAR</h1>
+        <>
+            <h1>CALENDAR</h1>
+            <div className='calendar-container'>
+                <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView='dayGridMonth'
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay',
+                }}
+                dateClick={handleDateClick}
+                events={events}
+                />
+            </div>
+        </>
     )
 }
+
+// npm install @fullcalendar/react @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/interaction
