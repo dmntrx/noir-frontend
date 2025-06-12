660af9798385b192920d5756fac25552533e1cb2
dmntrx <maryshkuran@mail.ru>
2025-06-13 00:43:53 +0300

    Home page: ui - stub

--- react-vite-project/src/pages/Home.jsx
+++ react-vite-project/src/pages/Home.jsx
@@ -1,5 +1,9 @@
 export default function Home() {
     return (
-        <h1>HOME</h1>
+        <>
+            <h1>HOME</h1>
+            <h2>Next updates available soon. 
+                Be ready to unleash your full potential and control your everything. </h2>
+        </>
     )
 }
