fb6f66f42b618b1708104c53f5d043a404f042c5
dmntrx <maryshkuran@mail.ru>
2025-06-15 01:53:13 +0300

    Profile page: user's data and working logout

--- react-vite-project/src/pages/Profile.jsx
+++ react-vite-project/src/pages/Profile.jsx
@@ -1,21 +1,41 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Profile.css';
 
 export default function Profile() {
-    // временная заглушка на пользователя
-    const user = {
-        username: 'User1',
-        email: 'user1@example.com',
+    
+    // получение user
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+        setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    // logout
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('user');
+        navigate('/login');
     };
+
+    if (!user) {
+        return <p>Loading...</p>;
+    }
 
     return (
         <>
             <h1>PROFILE</h1>
             <div className="profile-info">
-                <p><strong>Username:</strong> {user.username}</p>
+                {/* <p><strong>Username:</strong> {user.username}</p> */}
                 <p><strong>Email:</strong> {user.email}</p>
             </div>
             <div className="profile-actions">
-                <button className="logout-btn">Log out</button>
+                <button className="logout-btn" onClick={handleLogout}>Log out</button>
                 <buttom className="delete-btn">Delete Account</buttom>
             </div>
         </>
