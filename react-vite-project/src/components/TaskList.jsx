b6e5b11a7cf02b848f0b8442a37105d6d737f513
dmntrx <maryshkuran@mail.ru>
2025-06-20 20:29:36 +0300

    Today page: task, subtasks sorting by completed

--- react-vite-project/src/components/TaskList.jsx
+++ react-vite-project/src/components/TaskList.jsx
@@ -16,9 +16,14 @@ export default function TaskList({ tasks = [], allTasks = [], onToggle, onDelete
     }
 
     const sortedTasks = [...tasks].sort((a, b) => {
-        if (a.is_completed === b.is_completed) return 0;
-        return a.is_completed ? 1 : -1;
-    });
+        if (a.priority !== b.priority) {
+            return a.priority - b.priority; // сначала по приоритету (1, 2, 3)
+        }
+        if (a.is_completed === b.is_completed) {
+            return 0;
+        }
+        return a.is_completed ? 1 : -1; // выполненные ниже
+        });
 
   return (
     <ul>
@@ -27,12 +32,17 @@ export default function TaskList({ tasks = [], allTasks = [], onToggle, onDelete
           <TaskItem task={task} onToggle={onToggle} onDelete={onDelete} onEdit={onEdit} />
           <ul style={{ marginLeft: '1rem' }}>
             {allTasks
-              .filter((t) => t.parent_task_id === task.id)
-              .map((subtask) => (
-                <li key={subtask.id}>
-                  <TaskItem task={subtask} onToggle={onToggle} onDelete={onDelete} onEdit={onEdit} />
-                </li>
-              ))}
+                .filter((t) => t.parent_task_id === task.id)
+                .sort((a, b) => {
+                    if (a.is_completed === b.is_completed) return 0;
+                    return a.is_completed ? 1 : -1;
+                })
+                .map((subtask) => (
+                    <li key={subtask.id}>
+                    <TaskItem task={subtask} onToggle={onToggle} onDelete={onDelete} onEdit={onEdit} />
+                    </li>
+                ))}
+
           </ul>
         </li>
       ))}
